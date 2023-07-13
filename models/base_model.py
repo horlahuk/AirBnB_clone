aee162ca87dddaf85e7e0164d9ea4bb6837e38fc
horlahuk <olaitan.u.k@gmail.com>
2023-07-13 05:48:05 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,16 @@ from datetime import datetime
 
 class BaseModel():
     """class BaseModel"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialize instances"""
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value)
+                else:
+                    self.__dict__[key] = value
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
