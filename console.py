cca1e8974ee65009489d35857ebf8360986aab74
horlahuk <olaitan.u.k@gmail.com>
2023-07-15 09:08:03 -0700

    task 6

--- console.py
+++ console.py
@@ -7,4 +7,19 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand"""
-    pass
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """ Quits the console """
+        return True
+
+    def do_EOF(self, arg):
+        """ Quits the console """
+        return True
+
+    def emptyline(self):
+        """ Do nothing when enter is pressed """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
