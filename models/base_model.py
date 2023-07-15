cf72d2ff0154e12f67a292bdfd42e9c366267b36
horlahuk <olaitan.u.k@gmail.com>
2023-07-15 07:52:00 -0700

    fixed code

--- models/base_model.py
+++ models/base_model.py
@@ -20,10 +20,11 @@ class BaseModel():
                     self.__dict__[key] = datetime.fromisoformat(value)
                 else:
                     self.__dict__[key] = value
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """ String representation of print statement"""
