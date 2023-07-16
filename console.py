fe1ffb1953b89ee7cc41d2d014d863210ffd306b
horlahuk <olaitan.u.k@gmail.com>
2023-07-16 07:36:28 -0700

    updated file_storage and console for work with other class

--- console.py
+++ console.py
@@ -6,11 +6,16 @@ import cmd
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand"""
-    __classes = ["BaseModel", "User"]
+    __classes = ["BaseModel", "User", "Place", "State", "City", "Amenity", "Review"]
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
